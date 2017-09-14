adfa5c25e4091e8aac9ecb3e84b73a6109d97829
pytech-pc <690759006@qq.com>
2017-09-14 10:57:24 +0800

    baidupush

--- js/main.js
+++ js/main.js
@@ -58,3 +58,17 @@
 (function() {
     var demoItems = document.querySelectorAll('.grid-item')
 }());
+
+//百度蜘蛛主动推送
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';        
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
